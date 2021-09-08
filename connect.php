37d059b01ece4a9aea1fb55e4e59d0dfc1b5c4c1
Pedro H Lopes <85568654+pedrohlopesnvp@users.noreply.github.com>
2021-09-08 14:02:07 -0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,1 +1,34 @@
+<?php
+
+require_once 'connect.php';
+require_once 'dao/AlunoDaoMysql.php';
+
+
+$movDao = new AlunoDaoMysql($pdo);
+$lista = $movDao->findAll();
+
+
+?>
+
+
+<a href="addAluno.php">Adicionar Aluno</a><br><br>
+
+
+<?php foreach ($lista as $aluno):?>
+<table border="1" width="40%">
+    <tr>
+        Aluno <?=$aluno->getId();?>     
+    </tr>
+    <a href="updateAluno.php?id=<?=$aluno->getId();?>"> [Editar]  </a>
+    <a href="deleteAluno.php?id=<?=$aluno->getId();?>">  [Excluir]</a>
+    <tr>
+        <td><?=$aluno->getAluno();?></td>    
+    </tr>
+    <tr>
+        <td><?=$aluno->getAluno();?></td>
+    </tr>
+</table><br>
+<?php endforeach; ?>
+<br><br><br>
+
 
