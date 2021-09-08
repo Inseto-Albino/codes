c4cbf33db61878c24d5e367f8a9195f57876a902
Pedro H Lopes <85568654+pedrohlopesnvp@users.noreply.github.com>
2021-09-08 14:04:43 -0300

    Update deleteAluno.php

--- deleteAluno.php
+++ deleteAluno.php
@@ -1,1 +1,16 @@
+<?php
+
+require 'connect.php';
+require_once 'dao/AlunoDaoMysql.php';
+
+$alunoDao = new AlunoDaoMysql($pdo);
+
+$id = filter_input(INPUT_GET, 'id');
+
+if($id) {
+    $alunoDao->deleteAluno($id);
+}
+
+header("Location: index.php");
+exit;
 
