f930598754d8ba38ac26274187963f659709cfa9
Pedro H Lopes <85568654+pedrohlopesnvp@users.noreply.github.com>
2021-09-13 10:13:50 -0300

    Update updateAluno_action.php

--- updateAluno_action.php
+++ updateAluno_action.php
@@ -1,1 +1,31 @@
+<?php
+require_once 'connect.php';
+require_once 'dao/AlunoDaoMysql.php';
 
+
+$alunoDao = new AlunoDaoMysql($pdo);
+
+
+$alunoId = filter_input(INPUT_POST, 'id');
+$nomeDescription = filter_input(INPUT_POST, 'nome');
+$turmaDescription = filter_input(INPUT_POST, 'turma');
+
+
+
+if ($nomeDescription && $turmaDescription){
+    $aluno = $alunoDao->findById($alunoId);
+    $aluno->setNome($nomeDescription);
+    $aluno->setTurma($turmaDescription);
+
+    $alunoDao->updateAluno($aluno);   
+
+    header ("Location: index.php");
+    exit;
+}else {
+    header("Location: updateAluno.php?id=".$alunoId);
+exit;
+}
+
+
+
+?>
