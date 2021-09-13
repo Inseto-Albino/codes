08356222fbcc540ba728599447fb168d47dba280
Pedro H Lopes <85568654+pedrohlopesnvp@users.noreply.github.com>
2021-09-13 10:05:19 -0300

    Update addAluno.php

--- addAluno.php
+++ addAluno.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="POST" action="addAluno_action.php">
+    <label>
+        Aluno: 
+        <input type="text" name="nome">
+    </label><br><br>
+    <label>
+        Curso: 
+        <input type="text" name="curso">
+    </label><br><br>
+  
+    <input type="submit" value="Adicionar">
+
+</form>
+</body>
+</html>
 
