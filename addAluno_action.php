fdbbf4e13367c5f2d1dbcd285f6b981a7f26e300
Pedro H Lopes <85568654+pedrohlopesnvp@users.noreply.github.com>
2021-09-13 10:09:47 -0300

    Update addAluno_action.php

--- addAluno_action.php
+++ addAluno_action.php
@@ -1,1 +1,32 @@
+<?php
+require_once 'connect.php';
+require_once 'models/Aluno.php';
+require_once 'dao/AlunoDaoMysql.php';
+
+
+$alunoDao = new AlunoDaoMysql($pdo);
+
+
+$nomeDescription = filter_input(INPUT_POST, 'nome');
+$turmaDescription = filter_input(INPUT_POST, 'turma');
+
+
+
+if ($nomeDescription && $turmaDescription){
+    $aluno = new Question();
+    $aluno->setNome($nomeDescription);
+    $aluno->setTurma($turmaDescription);
+
+
+    $alunoDao->addAluno($aluno);   
+
+    header ("Location: ".$base);
+    exit;
+}
+header("Location: addAluno.php");
+exit;
+
+
+?>
+
 
