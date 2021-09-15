4a905f776e21671b9d261a5062426d76daeb93c0
Pedro H Lopes <85568654+pedrohlopesnvp@users.noreply.github.com>
2021-09-15 19:14:12 -0300

    Update addAluno.php

--- addAluno.php
+++ addAluno.php
@@ -12,11 +12,17 @@
         <input type="text" name="nome">
     </label><br><br>
     <label>
+        Curso: 
+        <input type="text" name="curso">
+    </label><br><br>
+    <label>
         Turma: 
         <input type="text" name="turma">
     </label><br><br>
   
     <input type="submit" value="Adicionar">
+    <br>
+    <a href="index.php">Voltar</a>
 
 </form>
 </body>
