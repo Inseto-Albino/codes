3bc43250f31ef8aecb03b3ca2cf04f6f8d632289
Pedro H Lopes <85568654+pedrohlopesnvp@users.noreply.github.com>
2021-09-15 19:17:06 -0300

    Update updateAluno.php

--- updateAluno.php
+++ updateAluno.php
@@ -1,33 +1,48 @@
 <?php
 require_once 'connect.php';
+require_once 'C:/xampp/htdocs/NovoSiga/models/Aluno.php';
 require_once 'C:/xampp/htdocs/NovoSiga/dao/AlunoDaoMysql.php';
 
-
 $alunoDao = new AlunoDaoMysql($pdo);
+$info = false;
+$id = filter_input(INPUT_GET, 'id');
 
-
-$alunoId = filter_input(INPUT_POST, 'id');
-$nomeDescription = filter_input(INPUT_POST, 'nome');
-$turmaDescription = filter_input(INPUT_POST, 'turma');
-$cursoDescription = filter_input(INPUT_POST, 'curso');
-
-
-
-if ($nomeDescription && $turmaDescription){
-    $aluno = $alunoDao->findById($alunoId);
-    $aluno->setNome($nomeDescription);
-    $aluno->setTurma($turmaDescription);
-    $aluno->setCurso($cursoDescription);
-
-    $alunoDao->updateAluno($aluno);   
-
-    header ("Location: index.php");
+if ($id) {
+    $info = $alunoDao->findById($id);
+}
+else {
+    header("Location: index.php");
     exit;
-}else {
-    header("Location: updateAluno.php?id=".$alunoId);
-exit;
 }
 
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="POST" action="updateAluno_action.php">
+    <input type="hidden" name="id" value="<?=$info->getId();?>">
+    <label>
+        Aluno: 
+        <input type="text" name="nome" value="<?=$info->getNome();?>">
+    </label><br><br>
+    <label>
+        Curso: 
+        <input type="text" name="curso" value="<?=$info->getCurso();?>">
+    </label><br><br>
+    <label>
+        Turma: 
+        <input type="text" name="turma" value="<?=$info->getTurma();?>">
+    </label><br><br>
+  
+    <input type="submit" value="Salvar">
+
+</form>
+</body>
+</html>
+
