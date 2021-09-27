63f0b8df0243121ef957cbb3068ef8130466c3cf
Pedro H Lopes <85568654+pedrohlopesnvp@users.noreply.github.com>
2021-09-27 18:10:47 -0300

    Update addAluno.php

--- addAluno.php
+++ addAluno.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<link rel="stylesheet" href="http://localhost/NovoSiga/geralCodes/style.php">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,7 +24,7 @@
   
     <input type="submit" value="Adicionar">
     <br>
-    <a href="index.php">Voltar</a>
+    <a href="alunoIndex.php">Voltar</a>
 
 </form>
 </body>
