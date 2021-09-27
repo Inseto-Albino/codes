20985280baff73eacfd14adfebf9ea8ae0f5a595
Pedro H Lopes <85568654+pedrohlopesnvp@users.noreply.github.com>
2021-09-27 18:11:24 -0300

    Update updateAluno.php

--- updateAluno.php
+++ updateAluno.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'connect.php';
-require_once 'C:/xampp/htdocs/NovoSiga/models/Aluno.php';
-require_once 'C:/xampp/htdocs/NovoSiga/dao/AlunoDaoMysql.php';
+require_once 'C:/xampp/htdocs/NovoSiga/alunoModels/Aluno.php';
+require_once 'C:/xampp/htdocs/NovoSiga/alunoDao/AlunoDaoMysql.php';
 
 $alunoDao = new AlunoDaoMysql($pdo);
 $info = false;
@@ -19,7 +19,8 @@ else {
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<link rel="stylesheet" href="http://localhost/NovoSiga/geralCodes/style.php"><head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
