90fb8de0a788a7be7e886691209411b739dcf782
Pedro H Lopes <85568654+pedrohlopesnvp@users.noreply.github.com>
2021-09-27 18:09:36 -0300

    Update and rename index.php to alunoIndex.php

--- alunoIndex.php
+++ alunoIndex.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'connect.php';
-require_once 'C:/xampp/htdocs/NovoSiga/dao/AlunoDaoMysql.php';
+require_once 'C:/xampp/htdocs/NovoSiga/alunoDao/AlunoDaoMysql.php';
 
 
 $movDao = new AlunoDaoMysql($pdo);
@@ -9,9 +9,11 @@ $lista = $movDao->findAll();
 
 
 ?>
-  
-<a href="addAluno.php">Adicionar Aluno</a><br><br>
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<link rel="stylesheet" href="http://localhost/NovoSiga/geralCodes/style.php">
+<a href="http://localhost/NovoSiga/geralCodes/index.php">Voltar</a><br><br>
+<a href="addAluno.php">Adicionar Aluno</a><br><br>
 
 <?php foreach ($lista as $aluno):?>
 <table border="1" width="40%">
